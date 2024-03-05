5576e60c3c94fe4c09ecf2f5ff98b3cc6c7cbd68
meetitaliya <meetitaliya010@gmail.com>
2024-03-05 11:01:59 +0530

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// let obj={
+//   name:"meet",
+//   role:"devloper"
+// }
+
+// for (const key in object) {
+//     const element = object[key];
+//     console.log(key,element)
+    
+// }
+  let i=0;
+while (i<6) {
+   console.log(i)  
+
+   i++;
+}
